6ed1a3ea96ebc87dd9db3152d8ad36180ae1ee94
avfrontend <avraam.kastrappis@gmail.com>
2022-08-10 21:18:34 +0300

    todo list typescript app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {legacy_createStore as createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {App} from './components/App'
+import {reducers} from './reducers'
 
-interface Props {
-    color?: string
-}
+const store = createStore(reducers,applyMiddleware(thunk)) 
 
-class App extends React.Component<Props> {
-    render() {
-        return <div>{this.props.color}</div>
-    }
-}
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>,
+    </Provider>,
+ document.querySelector('#root'))
